de88806a55d26e579d35d928e424689e44842537
qa-ltfox-1-rhnk@example.com <qa-ltfox-1-rhnk@example.com>
2023-05-15 16:18:59 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16400.stoplight-dev.com/api/v1/projects/cHJqOjI4NjY/spectral.js?branch_slug=main&token=69ee73a8-bae1-4788-81b1-4e87fad271fd";
+export default { extends: ruleset };
